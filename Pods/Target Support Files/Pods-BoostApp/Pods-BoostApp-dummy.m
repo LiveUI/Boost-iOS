ff7ccb0d64cb71f5b51374d868b6c49c3a723fc1
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2017-12-15 12:55:18 +0000

    removing pods

--- Pods/Target Support Files/Pods-BoostApp/Pods-BoostApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BoostApp : NSObject
-@end
-@implementation PodsDummy_Pods_BoostApp
-@end
--- Pods/Target Support Files/Pods-BoostAppTests/Pods-BoostAppTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_BoostAppTests : NSObject
-@end
-@implementation PodsDummy_Pods_BoostAppTests
-@end
