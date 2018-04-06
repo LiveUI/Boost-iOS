8a9c08822604475dad7a926efa3b83ad70c022f2
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-04-06 08:22:31 +0100

    wip

---
+++ BoostApp/Views/Headers/LeftMenuHeader.swift
@@ -0,0 +1,30 @@
+//
+//  LeftMenuHeader.swift
+//  BoostApp
+//
+//  Created by Ondrej Rafaj on 03/04/2018.
+//  Copyright © 2018 LiveUI. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Modular
+
+
+class LeftMenuHeader: UICollectionReusableView {
+    
+    var titleLabel = UILabel()
+    
+    // MARK: Initialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        titleLabel.place.on(self).with.leftMargin(6).rightMargin(6).topToBottom()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
