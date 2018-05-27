8d71ced242e216b1c4a9cb7ce468a69c743e6551
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2018-05-27 23:01:50 +0100

    Initial Commit

---
+++ Boost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boost
+//
+//  Created by Ondrej Rafaj on 27/05/2018.
+//  Copyright © 2018 LiveUI. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
